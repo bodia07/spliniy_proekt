4cdaae2a1879d85ad5fc55d9c992bbf426afe51c
bodia07 <bohdan.stefanyk07@gmail.com>
2024-05-20 19:04:46 +0200

    bug fix

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 let modal_btn=document.querySelector("#add_aply")
 let modal=document.querySelector(".modal-bg")
+let send=document.querySelector(".send")
 
 modal_btn.addEventListener("click",function(){
     modal.style.display="block"
 })
 
+send.addEventListener("click",function(){
+    modal.style="none"
+
+})
